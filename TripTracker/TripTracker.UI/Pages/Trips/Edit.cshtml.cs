2281f4f00e6d9a27b2d0e50ffd0af47286b16467
Prasanna Kumarasamy <kp.zeus@gmail.com>
2020-07-28 22:50:00 +0530

    Authz

--- TripTracker/TripTracker.UI/Pages/Trips/Edit.cshtml.cs
+++ TripTracker/TripTracker.UI/Pages/Trips/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,6 +13,7 @@ using TripTracker.UI.Services;
 
 namespace TripTracker.UI.Pages.Trips
 {
+    [Authorize]
     public class EditModel : PageModel
     {
         private readonly IApiClient _Client;
