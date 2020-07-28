a92b87f1a47b772fdc03847be84e51ff00b32805
Prasanna Kumarasamy <kp.zeus@gmail.com>
2020-07-28 14:19:38 +0530

    init

---
+++ TripTracker/TripTracker.Service/Models/Segment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.Service.Models
+{
+    public class Segment
+    {
+        public int Id { get; set; }
+
+        public int TripId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
---
+++ TripTracker/TripTracker.Service/Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.Service.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
